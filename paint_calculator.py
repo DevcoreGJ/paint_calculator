82717e4ae3a02fda232d6e837b7f19c479e8eb07
nic ledger <nicledgerallmail@gmail.com>
2022-11-14 20:25:05 +0000

    final

--- paint_calculator.py
+++ paint_calculator.py
@@ -3,34 +3,46 @@ from tkinter import *
 import tkinter as ttk
 from tkinter.messagebox import showinfo
 
-global errInvOp
-errInvOp = "Not a valid operator please choose from the buttons provided."
+#I decided for the design typing would be better.
 
+#Below are commented out OOP refactoring
+
+'''
+class App(tk.Tk):
+	def __init__(self):
+		super().__init__()
+
+		self.title('Paint Calculator')
+		self.resizable(False, False)
+		#self.geometry('400x300')
+
+class Find_Volume:
+	@staticmethod
+	def return_volume(walls_width, walls_length, walls_depth):
+   		#self.volume_val = ((float(self.walls_width.get())) * (float(self.walls_length.get())) * (float(self.walls_depth.get())))
+   		vol_val = ((float(walls_width)) * (float(walls_length)) * (float(walls_depth)))
+'''
 class Paint_calculator(tk.Tk):
 	def __init__(self):
 		super().__init__()
 
-		#validation checker referenced for def validate
-		# basically prevents the use of anything but numbers
+		#These are my buttons
 
 		self.get_area = ttk.Button(self, text = "area =", padx=40, pady=20, command= self.return_area)
 		self.paint_req = ttk.Button(self, text = "paint ml/m2' =", padx=40, pady=20,command= self.return_paint)
 		self.get_vol = ttk.Button(self, text = "volume=", padx=40, pady=20, command= self.return_volume)
+		
+		#These are my button locations
 
 		self.get_area.grid(row = 7, column = 0, columnspan = 1)
 		self.paint_req.grid(row = 7, column = 1, columnspan = 1)
 		self.get_vol.grid(row = 7, column = 2, columnspan = 1)
 
+		#stringvars allow me to convert vars to floats
 		self.walls_length = tk.StringVar()
 		self.walls_depth = tk.StringVar()
 		self.walls_width = tk.StringVar()
 		
-		#self.walls_length_west = tk.StringVar()
-
-		#self.walls_width_north = tk.StringVar()
-		#self.walls_width_east = tk.StringVar()
-		#self.walls_width_south = tk.StringVar()
-		#self.walls_width_west = tk.StringVar()
 		self.perimeter = tk.StringVar()
 		self.perimeter_val = tk.StringVar()
 		self.area = tk.StringVar()
@@ -42,11 +54,12 @@ class Paint_calculator(tk.Tk):
 		self.paint_req = tk.StringVar()
 
 
+		#labels and entry fields below
 		self.area_label= Label(self, text="Area of floor(metres)")
 		
 		self.area_val= Entry(self, textvariable=self.area, width=35, borderwidth= 5) #area of floor
 		self.perimeter_label= Label(self, text="Perimeter")
-		self.perimeter= Entry(self, textvariable=self.perimeter, width=10, borderwidth= 5, )
+		self.perimeter= Entry(self, textvariable=self.perimeter, width=10, borderwidth= 5)
 		
 		self.paint_req_label= Label(self, text="Paint required ml/m^2")
 		self.paint_req = Entry(self, textvariable=self.paint_req, width=35, borderwidth=5)
@@ -62,18 +75,6 @@ class Paint_calculator(tk.Tk):
 
 		self.depth_label = Label(self, text="depth of wall")
 		self.walls_depth = Entry(self,textvariable=self.walls_depth,width=10, borderwidth = 5)
-
-		
-		# define entry boxes
-		#walls_width_north =tk.Entry(self.panel2, validate = 'key', validatecommand = vcmd)
-
-		#validate the entry is a number
-
-		# define buttons
-
-		#self.button1 = Button(self, text = "calculate area of floor", padx=40, pady=20, command=lambda: buttonClick(1))
-
-		#self.buttonClear = Button(self, text = "Clear",padx=31,pady=20,command= buttonClear)
 
 		self.area_label.grid(row=0, column=0)
 		self.area_val.grid(row = 0, column = 1, columnspan = 5, padx=10, pady=10) #area of floor
@@ -97,41 +98,61 @@ class Paint_calculator(tk.Tk):
 		self.perimeter_label.grid(row= 4, column=2)
 		self.perimeter.grid(row=4, column=3)
 
-	def return_perimeter(self):
-		self.perimeter_val = ((float(self.walls_width.get())) + (float(self.walls_length.get()))) * 2
-		self.perimeter.insert(0,float(self.perimeter_val))
+		#my methods
+#this button function below did work but I broke it and ran out of time.
 
-		
+#when paint ml/m^2 button is clicked do:		
 	def return_area(self):
-
-		
-		#self.avg_width = (float(self.walls_width_north.get()) + float(self.walls_width_north.get()) + float(self.walls_width_south.get()) + float(self.walls_width_west.get())) // 4
-		#self.avg_width = self.walls_length_north = (float(entry_self.walls_length_north).get()+ self.walls_length_east = (float(self.area_valntry_walls_length_east).get()+ self.walls_length_south = float(self.area_valntry_walls_length_south).get()+ 
+		self.area_val.delete(0, END)#clears previous values for user friendiness.
+		self.perimeter.delete(0, END) #as coding in a rush some of variable names got switched about. Would definitely keep the naming convention in check
+		self.perimeter_val = ((float(self.walls_width.get())) + (float(self.walls_length.get()))) * 2 #thought it would make sense to get the perimter if we are getting the area.
+		self.perimeter.insert(0,float(self.perimeter_val)) 
 		
 		self.area = ((float(self.walls_width.get())) * (float(self.walls_length.get())))
-		#self.perimeter_val = ((float(self.walls_width.get())) + (float(self.walls_length.get()))) * 2
 
 		self.area_val.insert(0,float(self.area))
 
-		#return_perimeter()
-		
-
-		#self.avg_length = (float(self.walls_length_north.get()) + float(self.walls_length_north.get()) + float(self.walls_length_south.get()) + float(self.walls_length_west.get()))
-		#self.avg_width = (float(self.walls_width_north.get()) + float(self.walls_width_north.get()) + float(self.walls_width_south.get()) + float(self.walls_width_west.get())) // 4
-		 
-		#self.perimeter_val = ((float(self.walls_width.get())) + (float(self.walls_length.get()))) * 2
-		
-		#self.area_val.set(self.area)
+#when volume = button is clicked do:
 
 	def return_volume(self):
+
+		self.vol_room.delete(0, END)#clears previous values for user friendiness.
 		self.volume_val = ((float(self.walls_width.get())) * (float(self.walls_length.get())) * (float(self.walls_depth.get()))) *1
 		
 		self.vol_room.insert(0,float(self.volume_val))
 
+#when paint ml/m^2 button is clicked do:
+
 	def return_paint(self):
-		self.paint_val = ((float(self.walls_width.get())) * (float(self.walls_length.get())) * (float(self.walls_depth.get()))) - ((float(self.walls_width.get())))*50
+		self.paint_req.delete(0, END) #clears previous values for user friendiness.
+		self.paint_val = ((float(self.walls_width.get())) * (float(self.walls_length.get()))) * 10
+		
 		self.paint_req.insert(0,float(self.paint_val))
+
+
+#more oop refactoring below was going to try use the volume class as demo.
+
+'''
+def return_volume(self):
+		"""  Handle button click event
+		"""
+		try:
+			walls_width = float((self.walls_width.get()))
+			walls_length = float((self.walls_length.get()))
+			walls_depth = float((self.walls_depth.get()))
+			#vol_val = Find_Volume.return_volume(float(w),float(l),float(d))
+			#result = '{width} Volume = 
+			#result = Find_Volume.return_volume(self.volume_val)
+			#Find_Volume.return_volume(self.walls_width,self.walls_length,self.walls_depth)
+
+			self.vol_room.insert(0,float(Find_Volume.return_volume(walls_width,walls_length,walls_depth)))
+			#self.result_label.config(text=result)
+		except ValueError as error:
+			showerror(title='Error', message=error)
+'''
+
 #self.mainloop()
 if __name__ == "__main__":
+	#ConverterFrame(app)
 	app = Paint_calculator()
 	app.mainloop()
